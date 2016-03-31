d98a53c40894827d316061d7efaa30b0c9ef2a65
ragitam_6706144126 <rachmadagitam@users.noreply.github.com>
2016-03-31 10:25:59 +0700

    Update jurnal.java

--- jurnal.java
+++ jurnal.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 public class Jurnal1 {
     public static void main(String[]args){
-        int a = 1, b = 0, hasil = 1, c;
+        int a = 1, b = 2, hasil = 1, c;
         Scanner scn = new Scanner(System.in);
         System.out.print ("Masukan Deret Fibonacci  : ");
         c = scn.nextInt ();
