8fae0b96f28a266a69c7b856f6448796564f8e79
jurnalasik <rzackym@gmail.com>
2016-03-31 10:43:27 +0700

    Update jurnal.java

--- jurnal.java
+++ jurnal.java
@@ -3,6 +3,7 @@ public class Jurnal1 {
     public static void main(String[]args){
         int a = 1, b = 2, hasil = 1, c;
         Scanner scn = new Scanner(System.in);
+        System.out.println("Hasil Rollback Saya");
         System.out.print ("Masukan Deret Fibonacci  : ");
         c = scn.nextInt ();
         for(int i=1; i<=c; i++){           
