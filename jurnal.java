a72f750ed6059a1251e13de029708036ace7bc05
ragitam_6706144126 <rachmadagitam@users.noreply.github.com>
2016-03-31 10:16:47 +0700

    Create jurnal.java

---
+++ jurnal.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class Jurnal1 {
+    public static void main(String[]args){
+        int a = 1, b = 0, hasil = 1, c;
+        Scanner scn = new Scanner(System.in);
+        System.out.print ("Masukan Deret Fibonacci  : ");
+        c = scn.nextInt ();
+        for(int i=1; i<=c; i++){           
+            a = b;
+            b = hasil;
+            System.out.print(hasil+" ");
+            hasil = a+b;
+        }
+    } 
+}
